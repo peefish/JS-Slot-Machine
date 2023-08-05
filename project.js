7f0f1bafd37537b7b7fd542560bb5af6cc32a480
peefish <hellopeefish@gmail.com>
2023-08-05 12:15:28 -0700

    Final Slot Machine

--- project.js
+++ project.js
@@ -132,14 +132,29 @@ const getWinnings = (rows, bet, lines) => {
     return winnings;
 };
 
+const game = () => {
 
 let balance = deposit();
+
+while (true) {
+console.log("You have a balance of $" + balance);
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
+balance -= bet * numberOfLines;
 const reels = spin();
 const rows = transpose(reels);
 printRows(rows);
 const winnings = getWinnings(rows, bet, numberOfLines)
+balance += winnings;
 console.log("You won, $" + winnings.toString());
 
+if (balance <= 0) {
+    console.log("You ran out of money");
+    break;
+} 
+const playAgain = prompt("Do you want to play again? (y/n)? ");
+if (playAgain != "y") break;
+}
+};
 
+game();
